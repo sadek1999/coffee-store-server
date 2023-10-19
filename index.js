6f092b5c75152c10850d909a916b2c4b4c5d9a28
Sadek Ali <sadekali1530@gmail.com>
2023-10-19 22:44:53 +0600

    add

--- index.js
+++ index.js
@@ -32,12 +32,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection= client.db('coffeeDB').collection('coffee');
+
+    app.post('/coffee',async(req,res)=>{
+      const newCoffee=req.body;
+      const result=await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+      console.log(newCoffee)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
